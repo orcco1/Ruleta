817a9bff4031b66185010ef5643c356672c8809a
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2025-04-17 23:40:45 -0600

    Create logger

--- roulette.py
+++ roulette.py
@@ -9,18 +9,26 @@ blacks = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 roulette = greens + reds + blacks
 
 def run_roulette():
-    result = random.choice(roulette)
-    return result
-
-if __name__ == "__main__":
-    result = run_roulette()
+    number = random.choice(roulette)
 
     # Determine the color
-    if result in greens:
+    if number in greens:
         color = "green"
-    elif result in reds:
+    elif number in reds:
         color = "red"
     else:
         color = "black"
 
-    print(f"Number: {result}  Color: {color}")
+    result = (f"Number: {number}  Color: {color}")
+
+    return result
+
+def log_result():
+    result = run_roulette()  # Call the function to get the result
+    with open("Logs/log.txt", "a") as f:
+        f.write(result + "\n")
+
+
+if __name__ == "__main__":
+    log_result()
+
