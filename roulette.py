d10d18b8a70a4b2b9285ac22d138b24c9484a40b
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2025-04-17 23:21:13 -0600

    Roulette Structure

--- roulette.py
+++ roulette.py
@@ -1,15 +1,26 @@
+import random
+
 # Roulette number colors
-
 greens = [0]
+reds = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+blacks = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 
-reds = [
-    1, 3, 5, 7, 9, 12, 14, 16, 18,
-    19, 21, 23, 25, 27, 30, 32, 34, 36
-]
+# Create a single list of all roulette numbers
+roulette = greens + reds + blacks
 
-blacks = [
-    2, 4, 6, 8, 10, 11, 13, 15, 17,
-    20, 22, 24, 26, 28, 29, 31, 33, 35
-]
+def run_roulette():
+    result = random.choice(roulette)
+    return result
 
-def run():
+if __name__ == "__main__":
+    result = run_roulette()
+
+    # Determine the color
+    if result in greens:
+        color = "green"
+    elif result in reds:
+        color = "red"
+    else:
+        color = "black"
+
+    print(f"Number: {result}  Color: {color}")
